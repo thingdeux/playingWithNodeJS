7e6c190c22d54941a6839ff9539d56bdd80266ac
Joshua Johnson <thingster@gmail.com>
2016-09-11 20:44:02 -0700

    Trying out request/response patterns.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,18 @@
 /**
  * Created by josh on 9/11/16.
  */
-function start() {
+function start(request, response) {
     console.log("Request handler 'start' was called")
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World START");
+    response.end();
 }
 
-function upload() {
+function upload(request, response) {
     console.log("Request handler 'upload' was called.")
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World UPLOAD");
+    response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -1,12 +1,15 @@
 /**
  * Created by josh on 9/11/16.
  */
-function route(handle, pathname) {
+function route(handle, pathname, request, response) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname]();
+        handle[pathname](request, response);
     } else {
         console.log("No request handler found for " + pathname);
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not Found");
+        response.end();
     }
 }
 
